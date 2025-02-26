e596e43040525b17f65dd6273222b0fba730c96b
Zayed Shamlan <zayedbinshamlan@gmail.com>
2025-02-26 14:18:07 +0300

    Firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:talent_space_test/core/routing/app_router.dart';
 import 'package:talent_space_test/core/theming/app_colors.dart';
+import 'package:talent_space_test/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MyApp(appRouter: AppRouter()));
 }
 
